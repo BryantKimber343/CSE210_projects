2aca67174aa2dccd43e5b2be696854cf6c2b6491
Bryant Kimber <bryant.kimber@gmail.com>
2025-01-16 15:40:37 -0700

    finished prep3

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -1,9 +1,36 @@
 using System;
-
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
+        Random randomGenerator = new Random();
+        int number = randomGenerator.Next(1,101);
+        // Console.WriteLine($"You entered {number}");
+        int guess = 0;
+        int tries = 0;
+        
+        do
+        {
+            
+            Console.Write("What is your guess: ");
+            string guessString = Console.ReadLine();
+            guess = Convert.ToInt16(guessString);
+            if (number > guess)
+            {
+                Console.WriteLine("Higher");
+            } else if (number < guess)
+            {
+                Console.WriteLine("Lower");
+            } else
+            {
+                Console.WriteLine("Good job, you guessed it!");
+                number = guess;
+            }
+            
+        
+            tries++;
+            
+        } while (number != guess);
+        Console.WriteLine($"it took you {tries} times to guess it.");
     }
 }
