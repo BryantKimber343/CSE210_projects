86dfafc1011196908fc1dc6596d25e9fbcd5d28a
Bryant Kimber <bryant.kimber@gmail.com>
2025-01-22 20:41:01 -0700

    Completed lesson2 challenges

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Resume myResume = new Resume();
+        myResume._name = "Michael Jordan";
+
+        Job job1 = new Job();
+        job1._company = "McDonald's";
+        job1._jobTitle = "General Manager";
+        job1._startYear = 2015;
+        job1._endYear = 2017;
+        myResume._jobs.Add(job1);
+        
+
+        Job job2 = new Job();
+        job2._company = "Pizza Hut";
+        job2._jobTitle = "Delivery Driver";
+        job2._startYear = 2010;
+        job2._endYear = 2014;
+        myResume._jobs.Add(job2);
+
+        myResume.DisplayResume();
     }
 }
